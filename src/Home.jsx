bef4dbed95baa8e7309472ab7923f8d6a4bfbebc
cpshibinoppo <cpshibinoppo@gmail.com>
2023-08-24 06:59:10 +0530

    backup

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import  Nav  from './components/nav/Nav'
 function Home() {
   return (
     <div className='flex flex-row'>
-      <div className="bg-sky-400 h-screen w-1/5 ">
+      <div className="bg-sky-200 h-screen w-1/5 ">
         <Nav/>
       </div>
     </div>
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,15 +4,27 @@ import './Nav.scss'
 function Nav() {
   return (
     <section className='flex justify-center py-8'>
-        <div>
-            <div className='w-28 h-28 rounded-full overflow-hidden'>
-                <img className='w-full object-cover' src="https://images.unsplash.com/photo-1639747279286-c07eecb47a0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" alt="" />
+        <div className='flex flex-col items-center'>
+            <div className='w-20 h-20 rounded-full overflow-hidden relative border-light'>
+                <img className='w-full object-cover absolute -top-6' src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTR8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=80" alt="" />
             </div>
             <div>
                 <h3 className='text-center'><span className='relative italic block w-full text-5xl'>Robert Elisc <span className='absolute top-2/4 left-2/4 font-bold z-10 not-italic text-lg mr-1 w-full'>Robert Elisc</span></span></h3>
             </div>
         </div>
-    </section>
+        <div>
+          <nav>
+            <ul>
+              <li><a href="">Home</a></li>
+              <li><a href="">About</a></li>
+              <li><a href="">Service</a></li>
+              <li><a href="">Protfolio</a></li>
+              <li><a href="">Blog</a></li>
+              <li><a href="">Contact</a></li>
+            </ul>
+          </nav>
+        </div>
+    </section> 
   )
 }
 
